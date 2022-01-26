daa4e94ae6a0cf4aade978f4e5fcf387a1a5b68b
Fabio Regis <fabioregis1980@gmail.com>
2022-01-26 22:03:20 +0100

    Add life sciences graduates.

--- nodejs/prototype/anti-pattern/index.js
+++ nodejs/prototype/anti-pattern/index.js
@@ -27,9 +27,36 @@ engineerJane.addCourse("Physiscs 101");
 engineerJane.addCourse("Mathematics 201");
 engineerJane.addCourse("Principles of Ship Performance");
 
+const medicJohn = new Graduate();
+medicJohn.name = "John";
+medicJohn.subject = "Medicine";
+medicJohn.addCourse("Mathematics 101");
+medicJohn.addCourse("Chemistry 101");
+medicJohn.addCourse("Organic Chemistry 201");
+medicJohn.addCourse("Human Anatomy");
+
+const biochemistSarah = new Graduate();
+biochemistSarah.name = "Sarah";
+biochemistSarah.subject = "Biochemistry";
+biochemistSarah.addCourse("Mathematics 101");
+biochemistSarah.addCourse("Chemistry 101");
+biochemistSarah.addCourse("Organic Chemistry 201");
+biochemistSarah.addCourse("Macromolecular Structure Determination");
+
+const botanistJessica = new Graduate();
+botanistJessica.name = "Jessica";
+botanistJessica.subject = "Botany";
+botanistJessica.addCourse("Mathematics 101");
+botanistJessica.addCourse("Chemistry 101");
+botanistJessica.addCourse("Organic Chemistry 201");
+botanistJessica.addCourse("Plant Anatomy");
+
 const finish = new Date();
 
 console.log(JSON.stringify(engineerPaul));
 console.log(JSON.stringify(engineerPeter));
 console.log(JSON.stringify(engineerJane));
-console.log(`Time taken to create 3 graduates: ${(finish.getTime() - start.getTime()) / 1000} seconds.`);
+console.log(JSON.stringify(medicJohn));
+console.log(JSON.stringify(biochemistSarah));
+console.log(JSON.stringify(botanistJessica));
+console.log(`Time taken to create 6 graduates: ${(finish.getTime() - start.getTime()) / 1000} seconds.`);
--- nodejs/prototype/pattern/index.js
+++ nodejs/prototype/pattern/index.js
@@ -3,10 +3,19 @@ import { memoryUsage } from 'process';
 
 const start = new Date();
 
+// Create a prototype for engineers with the three basic courses
+// that all engineers must take.
 const engineerPrototype = new Graduate();
 engineerPrototype.addCourse("Mathematics 101");
 engineerPrototype.addCourse("Physiscs 101");
 engineerPrototype.addCourse("Mathematics 201");
+
+// Create a prototype for life sciences graduates with the three basic
+// courses that all life sciences graduates must take.
+const lifesciencesPrototype = new Graduate();
+lifesciencesPrototype.addCourse("Mathematics 101");
+lifesciencesPrototype.addCourse("Chemistry 101");
+lifesciencesPrototype.addCourse("Organic Chemistry 201");
 
 const engineerPaul = engineerPrototype.clone();
 engineerPaul.name = "Paul";
@@ -23,9 +32,27 @@ engineerJane.name = "Jane";
 engineerJane.subject = "Naval Engineering";
 engineerJane.addCourse("Principles of Ship Performance");
 
+const medicJohn = lifesciencesPrototype.clone();
+medicJohn.name = "John";
+medicJohn.subject = "Medicine";
+medicJohn.addCourse("Human Anatomy");
+
+const biochemistSarah = lifesciencesPrototype.clone();
+biochemistSarah.name = "Sarah";
+biochemistSarah.subject = "Biochemistry";
+biochemistSarah.addCourse("Macromolecular Structure Determination");
+
+const botanistJessica = lifesciencesPrototype.clone();
+botanistJessica.name = "Jessica";
+botanistJessica.subject = "Botany";
+botanistJessica.addCourse("Plant Anatomy");
+
 const finish = new Date();
-console.log(memoryUsage());
+
 console.log(JSON.stringify(engineerPaul));
 console.log(JSON.stringify(engineerPeter));
 console.log(JSON.stringify(engineerJane));
-console.log(`Time taken to create 3 graduates: ${(finish.getTime() - start.getTime()) / 1000} seconds.`);
+console.log(JSON.stringify(medicJohn));
+console.log(JSON.stringify(biochemistSarah));
+console.log(JSON.stringify(botanistJessica));
+console.log(`Time taken to create 6 graduates: ${(finish.getTime() - start.getTime()) / 1000} seconds.`);
