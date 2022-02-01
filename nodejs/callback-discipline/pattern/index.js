c2650c2666de0d8ac638c5ce3efa085af6622f4c
Fabio Regis <fabioregis1980@gmail.com>
2022-02-01 19:39:44 +0100

    eslinted js files

--- nodejs/callback-discipline/pattern/index.js
+++ nodejs/callback-discipline/pattern/index.js
@@ -1,12 +1,12 @@
-import { spider } from "./Spider.js";
+import { spider } from './Spider.js'
 
 // Take the url from the command line parameter process.argv[2]
 spider(process.argv[2], (err, filename, downloaded) => {
-    if (err) {
-        console.error(err);
-    } else if (downloaded) {
-        console.log(`Completed the download of "${filename}"`);
-    } else {
-        console.log(`"${filename}" was already downloaded`);
-    }
-});
+  if (err) {
+    console.error(err)
+  } else if (downloaded) {
+    console.log(`Completed the download of "${filename}"`)
+  } else {
+    console.log(`"${filename}" was already downloaded`)
+  }
+})
